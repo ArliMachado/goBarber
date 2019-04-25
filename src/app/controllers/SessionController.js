9f4b3446f323de4385c7ae83de0ffbb54279c922
Arli Machado <arli.machado@gmail.com>
2019-04-24 21:21:17 -0300

    Configurando a sessao da aplicação

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -17,7 +17,7 @@ class SessionController {
       console.log('Senha incorreta')
       return res.redirect('/')
     }
-
+    req.session.user = user
     return res.redirect('/app/dashboard')
   }
 }
